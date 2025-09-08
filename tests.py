dd4b7b246c92bc9efd3679d8732f778afe6967fe
anam-alam <anamit3010@gmail.com>
2025-09-08 13:44:00 +0530

    added new tests for run_python_file.py

--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@
 from functions.get_files_info import *
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
+
 
 
 
@@ -20,7 +22,13 @@ if __name__ == "__main__":
     # print(get_file_content("calculator", "pkg/calculator.py"))
     # print(get_file_content("calculator", "/bin/cat"))
     # print(get_file_content("calculator", "pkg/does_not_exist.py"))
-    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    # print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    # print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
     
