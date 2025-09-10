c67110d72c8b6b1d9e34362bbdb820bc0d17d895
anam-alam <anamit3010@gmail.com>
2025-09-10 12:19:47 +0530

    cleaned changes in tests.py

--- tests.py
+++ tests.py
@@ -17,11 +17,13 @@ if __name__ == "__main__":
     # print(get_files_info("calculator", "/bin")+"\n")
     # print("Result for '../' directory:")
     # print(get_files_info("calculator", "../")+"\n")
-    #print(get_file_content("calculator", "lorem.txt"))
+
+    # print(get_file_content("calculator", "lorem.txt"))
     # print(get_file_content("calculator", "main.py"))
     # print(get_file_content("calculator", "pkg/calculator.py"))
     # print(get_file_content("calculator", "/bin/cat"))
     # print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
     # print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
     # print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
     # print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
